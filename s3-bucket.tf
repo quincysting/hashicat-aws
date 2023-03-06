be5522696d15ab41a7d7669c3f32cda9dc0f8f75
YAN QIN <ian.qin@arq.group>
2023-03-06 10:05:00 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,10 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
-
+  
   bucket = "my-s3-bucket"
   acl    = "private"
+
+  bucket_prefix = "yanqin"
 
   versioning = {
     enabled = true
